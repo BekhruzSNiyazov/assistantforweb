3adb6cdfe7e905ec1920a17c1c10391932655689
CS50Niyazov <50236015+CS50Niyazov@users.noreply.github.com>
2019-11-25 18:43:53 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from random import randrange
 from speak import speak
-from getaudio import getaudio
 
 hello = ["Hello to you to!", "Hello! How are you?", "How can I help you?"]
 goodbye = ["Goodbye to you to!", "Have a nice day!", "Waiting for you..."]
@@ -18,159 +17,11 @@ def index():
                 <input name="message" id="send" placeholder="Type here...">
                 <button>Send</button>
             </form>
-            <form action="mic">
-                <button>microphone</button>
-            </form>
             """
 
 @app.route("/message")
 def assist():
     text = request.args.get("message")
-    if text:
-        text = text.lower()
-        if "hello" in text:
-            answer = hello[randrange(0, 2)]
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "goodbye" in text:
-            answer = goodbye[randrange(0, 2)]
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "random" in text and "number" in text:
-            answer = randrange(100)
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "/" in text:
-            try:
-                answer = f"{int(text.split()[0])} / {int(text.split()[-1])} = {int(text.split()[0])/int(text.split()[-1])}"
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-            except:
-                answer = sorry[randrange(0, 2)]
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "*" in text:
-            try:
-                answer = f"{int(text.split()[0])} * {int(text.split()[-1])} = {int(text.split()[0])*int(text.split()[-1])}"
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-            except:
-                answer = sorry[randrange(0, 2)]
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "+" in text:
-            try:
-                answer = f"{int(text.split()[0])} + {int(text.split()[-1])} = {int(text.split()[0])+int(text.split()[-1])}"
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-            except:
-                answer = sorry[randrange(0, 2)]
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "-" in text:
-            try:
-                answer = f"{int(text.split()[0])} - {int(text.split()[-1])} = {int(text.split()[0])-int(text.split()[-1])}"
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-            except:
-                answer = sorry[randrange(0, 2)]
-                speak(answer)
-                return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "how are you" in text:
-            answer = feeling[randrange(0, 2)]
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "what can you do" in text or "who are you" in text:
-            answer = "I am an Assistant by Bekhruz Niyazov. I can translate English to Russian, set timers, count and make you laugh."
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        elif "make me laugh" in text or "joke" in text:
-            answer = jokes[randrange(0, 1)]
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-        else:
-            answer = sorry[randrange(0, 2)]
-            speak(answer)
-            return render_template("index.html", answer=answer, user=text) + """
-            <form action="message" method="GET", "POST">
-                <input name="message" id="send" placeholder="Type here...">
-                <button onclick="send()">Send</button>
-            </form>
-            """
-
-@app.route("/mic")
-def mic():
-    speak("Listening")
-    text = getaudio()
     if text:
         text = text.lower()
         if "hello" in text:
